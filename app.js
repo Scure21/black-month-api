e4647a9ffb5e4685fdb8ae1a1b84233e35c8c604
seanlandis <sean.landis@willowtreeapps.com>
2019-02-18 17:31:35 -0500

    Added 'cors' to avoid CORS error

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const path = require('path')
 const morgan = require('morgan')
 const router = require('./router/router')
 const handleError = require('./utils/handleError')
 
 const app = express()
-
+app.use(cors())
 app.use('/', express.static(path.join(__dirname, "/public")))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
